bd055b0c3a1eda940915a8d27217c227e680a98d
Aurelius Wendelken <webtobesocial@gmail.com>
2023-12-09 13:53:12 +0100

    first draft with filter methods for user

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-// fetch('/data/baumkataster_bonn.updated.geojson', {
+let dataObject = null
+let cluster = null
+
 fetch('./data/baumkataster_flensburg.updated.geojson', {
   method: 'GET'
 })
@@ -6,32 +8,44 @@ fetch('./data/baumkataster_flensburg.updated.geojson', {
   return response.json()
 })
 .then((data) => {
-  addData(data)
+  renderPromise(data, 0)
 })
 .catch(function (error) {
   console.log(error)
-});
+})
 
+fetch('./data/flensburg_stadtteile.geojson', {
+    method: 'GET'
+})
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    addDistrictsLayer(data)
+})
+.catch(function (error) {
+    console.log(error)
+})
 
-const map = L.map('map').setView([54.7836, 9.4321], 13);
+const map = L.map('map').setView([54.7836, 9.4321], 13)
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+}).addTo(map)
 
-let geocoder = L.Control.Geocoder.nominatim();
+let geocoder = L.Control.Geocoder.nominatim()
 
 if (typeof URLSearchParams !== 'undefined' && location.search) {
     // parse /?geocoder=nominatim from URL
-    let params = new URLSearchParams(location.search);
-    let geocoderString = params.get('geocoder');
+    let params = new URLSearchParams(location.search)
+    let geocoderString = params.get('geocoder')
 
     if (geocoderString && L.Control.Geocoder[geocoderString]) {
-        console.log('Using geocoder', geocoderString);
-        geocoder = L.Control.Geocoder[geocoderString]();
+        console.log('Using geocoder', geocoderString)
+        geocoder = L.Control.Geocoder[geocoderString]()
     } else if (geocoderString) {
-        console.warn('Unsupported geocoder', geocoderString);
+        console.warn('Unsupported geocoder', geocoderString)
     }
 }
 
@@ -40,12 +54,12 @@ const osmGeocoder = new L.Control.geocoder({
     position: 'topright',
     placeholder: 'Adresse oder Ort',
     defaultMarkGeocode: false
-}).addTo(map);
+}).addTo(map)
 
 osmGeocoder.on('markgeocode', e => {
-    const bounds = L.latLngBounds(e.geocode.bbox._southWest, e.geocode.bbox._northEast);
-    map.fitBounds(bounds);
-});
+    const bounds = L.latLngBounds(e.geocode.bbox._southWest, e.geocode.bbox._northEast)
+    map.fitBounds(bounds)
+})
 
 
 const customIcon = L.icon({
@@ -55,7 +69,7 @@ const customIcon = L.icon({
     iconAnchor: [12, 41],
     tooltipAnchor: [4, -41],
     shadowSize: [45, 41]
-});
+})
 
 
 const blackIcon = L.icon({
@@ -65,26 +79,63 @@ const blackIcon = L.icon({
     iconAnchor: [12, 41],
     tooltipAnchor: [2, -41],
     shadowSize: [45, 41]
-});
+})
 
 
-function addData(data) {
-    let markers = L.markerClusterGroup({
-        zoomToBoundsOnClick: true,
-        disableClusteringAtZoom: 19
-    });
+const queryform = document.querySelector('#form')
+
+if (queryform.length) {
+    queryform.addEventListener('change', (e) => {
+        e.preventDefault()
+
+        const data = new FormData(queryform)
+        const districtId = parseInt(data.get('district'))
+
+        renderPromise(dataObject, districtId)
+    })
+}
+
+
+function addDistrictsLayer(data) {
+    L.geoJson(data, {
+      style: {
+        color: '#fff',
+        fillColor: '#185a44',
+        fillOpacity: 0.4,
+        opacity: 0.6,
+        weight: 1
+    }
+    }).addTo(map)
+}
+
+
+function renderPromise(data, districtId) {
+    dataObject = data
+
+    if (cluster) {
+        map.removeLayer(cluster)
+    }
 
     const geojsonGroup = L.geoJSON(data, {
+        filter: function (feature) {
+            if (districtId === 0) {
+                return true
+            }
+
+            if (feature.properties.district_id === districtId) {
+                return true
+            }   
+        },
         onEachFeature: function (feature, layer) {
             layer.on('click', function (e) {
                 document.querySelector('#filter').scrollTo({
                     top: 0,
                     left: 0
-                });
+                })
 
                 map.setView(e.latlng, 19)
 
-                document.querySelector('#details').classList.remove('hidden');
+                document.querySelector('#details').classList.remove('hidden')
 
                 if ('tree_type' in e.target.feature.properties) {
                     document.querySelector('#treeType').innerHTML = e.target.feature.properties.tree_type
@@ -134,11 +185,18 @@ function addData(data) {
             return L.marker(latlng, {icon: icon}).bindTooltip(label, {
                 permanent: false,
                 direction: 'top'
-            }).openTooltip();
+            }).openTooltip()
         }
     })
 
-    markers.addLayer(geojsonGroup)
-    map.addLayer(markers)
-    map.fitBounds(markers.getBounds(), {padding: [0, 0, 0, 0]})
+    cluster = L.markerClusterGroup({
+	      spiderfyOnMaxZoom: false,
+	      showCoverageOnHover: false,
+        disableClusteringAtZoom: 19,
+        maxClusterRadius: 40
+    })
+    cluster.addLayer(geojsonGroup)
+    map.addLayer(cluster)
+
+    map.fitBounds(cluster.getBounds(), {padding: [0, 0, 0, 0]})
 }
